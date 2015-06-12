53bc1dcb39c2f960ab52bc6a31057e5e99b89108
pinak <pinak.0605@gmail.com>
2015-06-12 18:59:26 +0530

    changes

--- db/migrate/20150612131816_not_null_issue.rb
+++ db/migrate/20150612131816_not_null_issue.rb
@@ -1,5 +1,7 @@
 class NotNullIssue < ActiveRecord::Migration
   def change
     change_column :user_notifications, :from_email, :string, :null => true
+    change_column :user_notifications, :from_name, :string, :null => true
+
   end
 end
